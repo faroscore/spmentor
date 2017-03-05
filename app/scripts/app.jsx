94a9c9820d31e0c9ce9c32184a13b73392a32a23
Григорий Соловьев <kefan8@mail.ru>
2017-03-05 18:46:37 +0300

    LabeledInput вынесен в отдельный модуль.

--- app/scripts/app.jsx
+++ app/scripts/app.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import LabeledInput from './labeledinput.jsx'
 
 export default class App extends React.Component{
 
@@ -128,77 +129,4 @@ export default class App extends React.Component{
 	}
 }
 
-class LabeledInput extends React.Component {
 
-	constructor() {
-		super();
-		this.validate = this.validate.bind(this);
-	}
-
-	validate(e){
-		let val = this.refs.input.value;
-
-		if (val === ""){
-			this.refs.input.classList.add("input_wrong");
-			throw new Error( e.target.getAttribute("name") + " is empty");
-		}
-
-		if (this.props.name.includes("name")){
-			if (!/([а-яА-Я]+(\ )+){2}([а-яА-Я]+){1}/.test(val)){
-				this.refs.input.classList.add("input_wrong");
-				throw new Error("fio should be constructed from 3 words");
-			}
-		}
-
-		if (this.props.name === "date"){
-			if (!/\d{2}.\d{2}.\d{4}/.test(val)){
-				this.refs.input.classList.add("input_wrong");
-				throw new Error("Date format should be in format DD.MM.YYYY");
-			}
-
-			let date = val.split(".");
-
-			if (+date[0] > 31 || +date[0] < 1 ){
-				this.refs.input.classList.add("input_wrong");
-				throw new Error("Date day should be between 1 and 31");
-			}
-
-			if (+date[1] > 12 || +date[1] < 1 ){
-				this.refs.input.classList.add("input_wrong");
-				throw new Error("Date month should be between 1 and 12");
-			}
-
-			if (+date[2] < 0 ){
-				this.refs.input.classList.add("input_wrong");
-				throw new Error("Date year should be above or equal zero");
-			}
-		}
-
-		if (this.props.name === "initials"){
-			if (!/([а-яА-Я]* [А-Я]\.[А-Я]\.)/.test(val)){
-				this.refs.input.classList.add("input_wrong");
-				throw new Error("Initials should be in format Surname.N.M.");
-			}
-
-		}
-
-		this.props.onChange();
-	}
-
-	render(){
-		return(
-				<div className="labeled-input">
-					<label>
-						<span>{this.props.caption}</span>
-						<input 
-							ref="input" 
-							name={this.props.name} 
-							placeholder={this.props.caption} 
-							type="text" 
-							onBlur={this.validate} 
-							onFocus={(e)=>{e.target.classList.remove("input_wrong")}}/>
-					</label>
-				</div>
-			)
-	}
-}
--- app/scripts/main.jsx
+++ app/scripts/main.jsx
@@ -3,6 +3,7 @@ import ReactDom from 'react-dom';
 import App from "./app.jsx";
 
 import "../styles/main.sass";
+
 ReactDom.render(<App/>,document.getElementById("root"));
 
 // export default
